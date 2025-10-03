eb1098c0d5e240eedc4c789c3de17a4ae1c45b5a
Oliwier <G00434405@atu.ie>
2025-10-03 12:47:03 +0100

    added division operator to Calculator.java

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -10,5 +10,8 @@ public class Calculator {
     public int mul(int a, int b) {
         return a * b;
     }
+    public int div(int a, int b) {
+        return a / b;
+    }
     // You will add subtract/multiply/divide later
 }
